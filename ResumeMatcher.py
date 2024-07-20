acb264620046082108145a631e447771578be696
Ankit160802 <142831535+Ankit160802@users.noreply.github.com>
2024-07-20 12:14:31 +0530

    Add files via upload

---
+++ ResumeMatcher.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+
+st.header("ResumeMatcher")
+
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+st.write(uploaded_file)
+# if uploaded_file is not None:
+#     loader=PyPDFLoader(uploaded_file)
+#     text=loader.load()
+#     whole_doc=text[0].page_content
+#     st.write(whole_doc)
+
