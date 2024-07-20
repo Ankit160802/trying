07312ff217a6caa837c54f9dbe07d414a1154509
Ankit160802 <142831535+Ankit160802@users.noreply.github.com>
2024-07-20 12:19:09 +0530

    Update ResumeMatcher.py

--- ResumeMatcher.py
+++ ResumeMatcher.py
@@ -6,9 +6,9 @@ st.header("ResumeMatcher")
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 st.write(uploaded_file)
-# if uploaded_file is not None:
-#     loader=PyPDFLoader(uploaded_file)
-#     text=loader.load()
-#     whole_doc=text[0].page_content
-#     st.write(whole_doc)
+if uploaded_file is not None:
+    loader=PyPDFLoader(uploaded_file)
+    text=loader.load()
+    whole_doc=text[0].page_content
+    st.write(whole_doc)
 
